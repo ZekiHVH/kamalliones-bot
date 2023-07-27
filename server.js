5c7aebce20c949049182884d1ae2d9c2f3d9701f
Zeki <iziquizel@gmail.com>
2023-07-27 13:28:07 -0300

    fix(server): typo

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.listen(process.env.PORT, () => ) {
-	console.log(`MOCK EXPRESS SERVER RUNNING ON PORT ${process.env.PORT}`);
-}
+app.listen(process.env.PORT, () => {
+  console.log(`MOCK EXPRESS SERVER RUNNING ON PORT ${process.env.PORT}`);
+});
